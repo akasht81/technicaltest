248aff75179d675ba65b68f051d6537f75168089
AKASH <akasht81@gmail.com>
2024-12-13 12:04:33 +0800

    added .gitignore

--- ASR/search-ui/packages/react-search-ui/src/SearchContext.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { SearchProviderContextInterface } from "./SearchProvider";
-
-const SearchContext =
-  React.createContext<SearchProviderContextInterface | null>(null);
-
-export default SearchContext;
